1c4c2a89cbef6d8146be7114316f446138cd13b0
Edward Williams <me@edwardwilliams.geek.nz>
2016-11-06 22:04:18 +1300

    models

--- horus/models.py
+++ horus/models.py
@@ -1,1 +1,26 @@
 from horus import app
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+                self.name, self.fullname, self.password)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    slug = Column(String, unique=True)
+    content = Column(String)
+    html = Column(String)
